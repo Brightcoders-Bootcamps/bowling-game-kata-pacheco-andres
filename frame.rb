e78f93e053f367116e3d837fb9ab7abda43f8373
Andres Pacheco <andrespach4@gmail.com>
2020-06-26 00:45:05 -0500

    I worked in the realization of my code based on TDD, it remains to go through rubocop

---
+++ frame.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+# frame logic class
+class Frame
+    def initialize
+      @turns = []
+    end
+    
+    def turns_completed?
+      @turns.size == 2
+    end
+    
+    def strike?
+      @turns.size == 1 && @turns.sum == 10
+    end
+    
+    def spare?
+      @turns.sum == 10
+    end
+    
+    def add(pin_down)
+      @turns << pin_down
+    end
+  
+    def turns
+      @turns
+    end
+  end
