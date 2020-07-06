722de051bfdd47c75995a604b8189600a673acc4
Andres Pacheco <andrespach4@gmail.com>
2020-07-06 17:41:03 -0500

    Attend rubycritic comments

--- bowling_game.rb
+++ bowling_game.rb
@@ -23,58 +23,82 @@ class BowlingGame
       game_roll.add(pins_down)
       frame = Frame.new
 
-      if frames.count == 10
-        strike_or_spare(frame, pins_down)
-      elsif game_roll.strike? && frames.count <= 9
-        frame.add(pins_down)
-        frames << frame
-        game_roll.reset
-      elsif game_roll.turns_completed? && frames.count <= 9
-        frame.add(game_roll.first_turn_pins_down)
-        frame.add(game_roll.second_turn_pins_down)
-        frames << frame
-        game_roll.reset
-      end
+      frame_type_validator(game_roll, pins_down, frame)
     end
+  end
+
+  def frame_type_validator(game_roll, pins_down, frame)
+    if frames_count == 10
+      strike_or_spare(frame, pins_down)
+    elsif game_roll.strike? && frames_count <= 9
+      frame.add(pins_down)
+      frames << frame
+      game_roll.reset
+    elsif game_roll.turns_completed? && frames_count <= 9
+      add_game_roll_to_frame(frame, game_roll)
+    end
+  end
+
+  def frames_count
+    frames.count
+  end
+
+  def add_game_roll_to_frame(frame, game_roll)
+    frame.add(game_roll.first_turn_pins_down)
+    frame.add(game_roll.second_turn_pins_down)
+    frames << frame
+    game_roll.reset
   end
 
   def strike_or_spare(frame, pins_down)
-    if frames.last.strike? || frames.last.spare?
-      frame.add(pins_down)
-      frames << frame
-    end
+    return unless frames.last.strike? || frames.last.spare?
+
+    frame.add(pins_down)
+    frames << frame
   end
 
   def score
-    score = 0
+    total_score = 0
     bonus = 0
-    
-    frames.each.with_index do |frame, index|
-      score += frame.turns.sum
-      
-      if index == 9 && (frame.strike? || frame.spare?)
-        bonus += frames[index + 1].turns.first
-      elsif frame.strike?
-        if frames[index + 1].strike?
-          bonus += frames[index + 1].turns.sum
-          bonus += frames[index + 2].turns.first
-        else
-          bonus += frames[index + 1].turns.sum
-        end
-      elsif frame.spare?
-        bonus += frames[index + 1].turns.first
-      end
 
-      score += bonus
+    frames.each.with_index do |frame, index|
+      total_score += frame.turns.sum
+
+      bonus = bonus_calculation(frame, index, bonus)
+      total_score += bonus
       bonus = 0
     end
 
-    score
+    total_score
+  end
+
+  def bonus_calculation(frame, index, bonus)
+    if index == 9 && (frame.strike? || frame.spare?)
+      bonus += frames_index_position(index).turns.first
+    elsif frame.strike?
+      bonus += strike_score(bonus, index)
+    elsif frame.spare?
+      bonus += frames_index_position(index).turns.first
+    end
+
+    bonus
+  end
+
+  def strike_score(bonus, index)
+    if frames_index_position(index).strike?
+      bonus += frames[index + 2].turns.first
+    end
+
+    bonus + frames_index_position(index).turns.sum
+  end
+
+  def frames_index_position(index)
+    frames[index + 1]
   end
 end
 
- game = BowlingGame.new
- game.start.each do |frame|
-   print frame.turns
- end
- puts "score #{game.score}"
+# game = BowlingGame.new
+# game.start.each do |frame|
+# print frame.turns
+# end
+# puts "score #{game.score}"
